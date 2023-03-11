cbf87d0adf154961f0d07a41b0c11cb07d89bfdc
dek0058 <editionunit@gmail.com>
2023-03-11 21:11:46 +0900

    #Add HUD추가

---
+++ Source/SProject/Housing/HousingHUD.cpp
@@ -0,0 +1,3 @@
+#include "HousingHUD.h"
+
+
---
+++ Source/SProject/Housing/HousingHUD.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "HousingHUD.generated.h"
+
+UCLASS()
+class SPROJECT_API AHousingHUD : public AHUD
+{
+    GENERATED_BODY()
+
+public:
+
+    
+
+};
