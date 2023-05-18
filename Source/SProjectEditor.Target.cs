cdd363d6b43b2aa803205b03a3cfadfce8b4004f
dek0058 <editionunit@gmail.com>
2023-05-18 19:30:22 +0900

    #Modify 언리얼 버전 5.1 -> 5.2로 변경

--- Source/SProjectEditor.Target.cs
+++ Source/SProjectEditor.Target.cs
@@ -9,7 +9,7 @@ public class SProjectEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
 		ExtraModuleNames.AddRange(
 			new string[]
 			{
