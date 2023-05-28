70fd4f8fc6d37bbfb3ffab639729c6240962e3b6
dek0058 <editionunit@gmail.com>
2023-05-28 20:11:26 +0900

    #Add 캐릭터 작업

--- Plugins/RTSUnitTemplate/Source/RTSUnitTemplate/Private/Animations/UnitBaseAnimInstance.cpp
+++ Plugins/RTSUnitTemplate/Source/RTSUnitTemplate/Private/Animations/UnitBaseAnimInstance.cpp
@@ -107,5 +107,4 @@ void UUnitBaseAnimInstance::SetBlendPoints(AUnitBase* Unit, float Deltaseconds)
 		
 		
 	}
-
 }
---
+++ Source/SProject/Character/Unit.h
@@ -0,0 +1,15 @@
+
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "Unit.generated.h"
+
+UCLASS()
+class SPROJECT_API AUnit : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    
+};
