65073a8b8fecb4ad9e9645d76a75d8d6f0c2aa6b
dek0058 <editionunit@gmail.com>
2024-04-13 16:04:31 +0900

    어트리뷰트 추가

--- Source/SProject/AbilitySystem/Attributes/UnitAttributeSet.h
+++ Source/SProject/AbilitySystem/Attributes/UnitAttributeSet.h
@@ -6,11 +6,26 @@
 
 #include "UnitAttributeSet.generated.h"
 
+#define UNIT_ATTRIBUTE_ACCESSORS(PropertyName) ATTRIBUTE_ACCESSORS(UUnitAttributeSet, PropertyName)
+
 UCLASS(BlueprintType, ClassGroup = Unit, Category = AbilityAttribute)
 class UUnitAttributeSet : public UMyAttributeSet
 {
     GENERATED_BODY()
 
 public:
+    UNIT_ATTRIBUTE_ACCESSORS(Life);
+    UNIT_ATTRIBUTE_ACCESSORS(Damage);
+    UNIT_ATTRIBUTE_ACCESSORS(Armor);
 
+
+private:
+    UPROPERTY(BlueprintReadOnly, Meta = (AllowPrivateAccess = true))
+    FGameplayAttributeData Life = 1.0F;
+
+    UPROPERTY(BlueprintReadOnly, Meta = (AllowPrivateAccess = true))
+    FGameplayAttributeData Damage = 0.0F;
+
+    UPROPERTY(BlueprintReadOnly, Meta = (AllowPrivateAccess = true))
+    FGameplayAttributeData Armor = 0.0F;
 };
