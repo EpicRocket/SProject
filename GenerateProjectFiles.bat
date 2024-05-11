4a02ff1c8e25322a530ee281e839d161c0f577ac
dek0058 <editionunit@gmail.com>
2024-05-11 18:07:26 +0900

    배치파일 갱신

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,1 +1,12 @@
-"C:/UnrealEngine/Engine/Build/BatchFiles/Build.bat" -projectfiles -project="%~dp0SProject.uproject" -game -engine -progress
+setlocal
+
+@REM 언리얼 엔진 경로
+SET UnrealEngine=C:/UnrealEngine
+
+@REM 프로젝트 이름
+SET Project=SProject
+
+SET UE="%UnrealEngine%/Engine/Build/BatchFiles/Build.bat"
+SET PROJECT="%~dp0%Project%.uproject"
+
+%UE% -projectfiles -project=%PROJECT% -game -engine -progress
---
+++ UpadeEpicRocket.bat
@@ -0,0 +1,10 @@
+﻿setlocal
+
+SET SP_CONFIG=%~dp0SProjectConfig.yaml
+
+IF NOT EXIST "%UE_CONFIG%" (
+    ECHO UnrealEngineConfig.yaml not found. Please run EpicRocket.bat to generate it.
+    GOTO :EOF
+)
+
+"ncdownloader.exe" "%SP_CONFIG%"
