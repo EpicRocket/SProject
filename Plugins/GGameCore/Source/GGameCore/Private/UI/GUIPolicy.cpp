9312e452aafe57785b76f42a258ed28c7f4cdffb
dek0058 <dek0058@naver.com>
2024-05-25 21:09:29 +0900

    게임 코어 작업

--- Plugins/GGameCore/Source/GGameCore/Private/UI/GUIPolicy.cpp
+++ Plugins/GGameCore/Source/GGameCore/Private/UI/GUIPolicy.cpp
@@ -1,10 +1,36 @@
 ﻿
 #include "UI/GUIPolicy.h"
-
+#include "GameFramework/PlayerController.h"
 #include "Core/GLocalPlayer.h"
+#include "UI/PrimaryGameLayout.h"
 
 void UGUIPolicy::NotifyPlayerAdded(UGLocalPlayer* LocalPlayer)
 {
+	check(LocalPlayer);
+	LocalPlayer->OnPlayerControllerSet.AddWeakLambda(this, [this](UGLocalPlayer* LocalPlayer, AGPlayerController* PlayerController)
+	{
+		NotifyPlayerRemoved(LocalPlayer);
+
+		if (FRootViewportLayoutInfo* LayoutInfo = RootViewportLayouts.FindByKey(LocalPlayer))
+		{
+			AddLayoutToViewport(LocalPlayer, LayoutInfo->RootLayout);
+			LayoutInfo->bAddedToViewport = true;
+		}
+		else
+		{
+			CreateLayoutWidget(LocalPlayer);
+		}
+	});
+
+	if (FRootViewportLayoutInfo* LayoutInfo = RootViewportLayouts.FindByKey(LocalPlayer))
+	{
+		AddLayoutToViewport(LocalPlayer, LayoutInfo->RootLayout);
+		LayoutInfo->bAddedToViewport = true;
+	}
+	else
+	{
+		CreateLayoutWidget(LocalPlayer);
+	}
 }
 
 void UGUIPolicy::NotifyPlayerRemoved(UGLocalPlayer* LocalPlayer)
@@ -13,4 +39,24 @@ void UGUIPolicy::NotifyPlayerRemoved(UGLocalPlayer* LocalPlayer)
 
 void UGUIPolicy::NotifyPlayerDestroyed(UGLocalPlayer* LocalPlayer)
 {
+}
+
+void UGUIPolicy::AddLayoutToViewport(UGLocalPlayer* LocalPlayer, UPrimaryGameLayout* Layout)
+{
+	UE_LOG(LogTemp, Log, TEXT("[%s]가 [%s] 플레이어의 루트 레이아웃 [%s]를 뷰포트에 추가하는 중입니다."), *GetName(), *GetNameSafe(LocalPlayer), *GetNameSafe(Layout));
+
+}
+
+void UGUIPolicy::CreateLayoutWidget(UGLocalPlayer* LocalPlayer)
+{
+	if (APlayerController* PlayerController = LocalPlayer->GetPlayerController(GetWorld()); IsValid(PlayerController))
+	{
+		TSubclassOf<UPrimaryGameLayout> LayoutWidgetClass = LayoutClass.LoadSynchronous();
+		if (ensure(LayoutWidgetClass && !LayoutWidgetClass->HasAnyClassFlags(CLASS_Abstract)))
+		{
+			UPrimaryGameLayout* NewLayoutObject = CreateWidget<UPrimaryGameLayout>(PlayerController, LayoutWidgetClass);
+			RootViewportLayouts.Emplace(FRootViewportLayoutInfo{ .LocalPlayer = LocalPlayer, .RootLayout = NewLayoutObject, .bAddedToViewport = true });
+			AddLayoutToViewport(LocalPlayer, NewLayoutObject);
+		}
+	}
 }
--- Plugins/GGameCore/Source/GGameCore/Public/UI/GUIPolicy.h
+++ Plugins/GGameCore/Source/GGameCore/Public/UI/GUIPolicy.h
@@ -6,6 +6,26 @@
 #include "GUIPolicy.generated.h"
 
 class UGLocalPlayer;
+class AGPlayerController;
+class UPrimaryGameLayout;
+
+USTRUCT()
+struct FRootViewportLayoutInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Transient)
+	TObjectPtr<UGLocalPlayer> LocalPlayer = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UPrimaryGameLayout> RootLayout = nullptr;
+
+	UPROPERTY(Transient)
+	bool bAddedToViewport = false;
+
+	bool operator==(const UGLocalPlayer* Other) const {return LocalPlayer == Other;}
+};
 
 UCLASS(Abstract, Blueprintable, Within = GUIManagerSubsystem)
 class GGAMECORE_API UGUIPolicy : public UObject
@@ -16,4 +36,16 @@ public:
 	void NotifyPlayerAdded(UGLocalPlayer* LocalPlayer);
 	void NotifyPlayerRemoved(UGLocalPlayer* LocalPlayer);
 	void NotifyPlayerDestroyed(UGLocalPlayer* LocalPlayer);
+
+protected:
+	void AddLayoutToViewport(UGLocalPlayer* LocalPlayer, UPrimaryGameLayout* Layout);
+
+	void CreateLayoutWidget(UGLocalPlayer* LocalPlayer);
+
+private:
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true))
+	TSoftClassPtr<UPrimaryGameLayout> LayoutClass;
+
+	UPROPERTY(Transient)
+	TArray<FRootViewportLayoutInfo> RootViewportLayouts;
 };
