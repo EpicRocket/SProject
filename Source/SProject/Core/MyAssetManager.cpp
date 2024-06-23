853d8f53911378a0256adda064d6b525e23cc1b0
dek0058 <editionunit@gmail.com>
2024-06-24 00:25:38 +0900

    AssetManager 추가

---
+++ Source/SProject/Core/MyAssetManager.cpp
@@ -0,0 +1,12 @@
+﻿
+#include "MyAssetManager.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(MyAssetManager)
+
+
+void UMyAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+    
+}
---
+++ Source/SProject/Core/MyAssetManager.h
@@ -0,0 +1,21 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "Templates/SubclassOf.h"
+
+#include "MyAssetManager.generated.h"
+
+UCLASS(Config = SProject)
+class SPROJECT_API UMyAssetManager : public UAssetManager
+{
+    GENERATED_BODY()
+
+public:
+	virtual void StartInitialLoading() override;
+
+
+private:
+	
+};
