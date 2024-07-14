d87b5f7b75c75444b7d5aebb48de427bd9cd5d9a
cannot206 <editionunit@gmail.com>
2024-07-14 17:22:19 +0900

    StageTable 추가, Default 스테이지 레벨 1로 변경

---
+++ Source/SProject/Table/StageTable.h
@@ -0,0 +1,22 @@
+// This is an automatically generated file. Do not modify it manually.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/EnumRange.h"
+#include "Engine/DataTable.h"
+#include "StageTable.generated.h"
+
+class UWorld;
+
+USTRUCT(BlueprintType)
+struct SPROJECT_API FStageTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Level = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UWorld> Map = nullptr;
+};
+
