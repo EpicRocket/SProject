de0ebdc54cab47ffc853ef239778340a711c2085
cannot206 <editionunit@gmail.com>
2024-07-27 00:48:15 +0900

    터치 시 프로세스 구상중

---
+++ Source/SProject/Types/PlayerTypes.h
@@ -0,0 +1,14 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "PlayerTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EPlayerControlleMode : uint8
+{
+    None,
+    PlayMode,
+    TowerBuildMode,
+};
