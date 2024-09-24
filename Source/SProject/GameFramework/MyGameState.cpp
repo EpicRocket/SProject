4b69069e23dcbc526b74fdb1ec6b5585d478b85b
cannot206 <editionunit@gmail.com>
2024-09-25 01:29:22 +0900

    스테이지 프리로드 구현

--- Source/SProject/GameFramework/MyGameState.cpp
+++ Source/SProject/GameFramework/MyGameState.cpp
@@ -1,19 +1,5 @@
 ﻿
 #include "MyGameState.h"
-#include "AbilitySystem/GAbilitySystemComponent.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(MyGameState)
 
-FName AMyGameState::AbilitySystemComponentName = TEXT("AbilitySystemComponent");
-
-AMyGameState::AMyGameState()
-{
-	AbilitySystemComponent = FObjectInitializer::Get().CreateDefaultSubobject<UGAbilitySystemComponent>(this, AMyGameState::AbilitySystemComponentName);
-	AbilitySystemComponent->SetIsReplicated(false);
-	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
-}
-
-UAbilitySystemComponent* AMyGameState::GetAbilitySystemComponent() const
-{
-	return AbilitySystemComponent;
-}
--- Source/SProject/GameFramework/MyGameState.h
+++ Source/SProject/GameFramework/MyGameState.h
@@ -2,29 +2,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/ObjectMacros.h"
-#include "Engine/EngineTypes.h"
-#include "GameFramework/GameState.h"
-#include "AbilitySystemInterface.h"
+#include "Core/GGameState.h"
 
 #include "MyGameState.generated.h"
 
 class UGAbilitySystemComponent;
 
 UCLASS(BlueprintType, Blueprintable)
-class MY_API AMyGameState : public AGameState, public IAbilitySystemInterface
+class MY_API AMyGameState : public AGGameState
 {
     GENERATED_BODY()
 
-    static FName AbilitySystemComponentName;
-
 public:
-    AMyGameState();
-
-    //~IAbilitySystemInterface
-    virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
-    //~End of IAbilitySystemInterface
-
-private:
-    TObjectPtr<UGAbilitySystemComponent> AbilitySystemComponent;
 };
