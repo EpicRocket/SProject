23b04e7635f28ca25b99ce2ff34d5a52f623fce3
jinyoung.jang <msnletsgo@gmail.com>
2024-11-24 03:20:53 +0900

    AttributeSet 생성

---
+++ Plugins/GGameCore/Source/GGameCore/Private/AbilitySystem/Attributes/GAttributeSet.cpp
@@ -0,0 +1,25 @@
+#include "AbilitySystem/Attributes/GAttributeSet.h"
+
+#include "AbilitySystem/GAbilitySystemComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(GAttributeSet)
+
+class UWorld;
+
+
+UGAttributeSet::UGAttributeSet()
+{
+}
+
+UWorld* UGAttributeSet::GetWorld() const
+{
+	const UObject* Outer = GetOuter();
+	check(Outer);
+
+	return Outer->GetWorld();
+}
+
+UGAbilitySystemComponent* UGAttributeSet::GetGAbilitySystemComponent() const
+{
+	return Cast<UGAbilitySystemComponent>(GetOwningAbilitySystemComponent());
+}
