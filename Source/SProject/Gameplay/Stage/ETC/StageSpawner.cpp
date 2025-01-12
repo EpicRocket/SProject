cbff9dcac67ab202a4d8c28d3265b71573d8f342
jinyoung.jang <msnletsgo@gmail.com>
2025-01-12 17:09:14 +0900

    Spawner 작업 부분 추가

--- Source/SProject/Gameplay/Stage/ETC/StageSpawner.cpp
+++ Source/SProject/Gameplay/Stage/ETC/StageSpawner.cpp
@@ -1,3 +1,40 @@
 ﻿
 #include "StageSpawner.h"
+#include "AIController.h"
+#include "Gameplay/ETC/GameplayPathActor.h"
+#include "Engine/World.h"
+#include "Unit/UnitCharacter.h"
+#include "Kismet/GameplayStatics.h"
 
+void AStageSpawner::Spawn()
+{
+	UE_LOG(LogTemp, Log, TEXT("StageSpawner spawn start."));
+	if (!GetWorld()) 
+	{
+		return;
+	}
+
+	TArray<AActor*> GameplayPathActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AGameplayPathActor::StaticClass(), GameplayPathActors);
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	if (!SpawnUnits.IsEmpty())
+	{
+		AUnitCharacter* Spawned = GetWorld()->SpawnActor<AUnitCharacter>(SpawnUnits[0], GetActorLocation(), GetActorRotation(), SpawnParams);
+
+		Spawned->SetGenericTeamId(GetGenericTeamId());
+	
+		AAIController* AIController = GetWorld()->SpawnActor<AAIController>();
+		if (AIController)
+		{
+			AIController->Possess(Cast<APawn>(Spawned));
+		}
+	}
+}
+
+int32 AStageSpawner::GetCurrentWave()
+{
+	return CurrentWave;
+}
--- Source/SProject/Gameplay/Stage/ETC/StageSpawner.h
+++ Source/SProject/Gameplay/Stage/ETC/StageSpawner.h
@@ -7,6 +7,9 @@
 
 #include "StageSpawner.generated.h"
 
+class AUnitCharacter;
+class AGameplayPathActor;
+
 UCLASS(Abstract, BlueprintType, Blueprintable)
 class MY_API AStageSpawner : public AGameplayTeamActor, public IGameplayActorTag
 {
@@ -16,4 +19,22 @@ class MY_API AStageSpawner : public AGameplayTeamActor, public IGameplayActorTag
     // - 1. 이 Spawner의 팀을 따라가도록 즉시 스폰
     // - 2. 스폰이 되자마자 AIController를 빙의
     // - 3. 특정 GameplayPathActor를 찾아야 함 ( 스폰 되자마자 자신의 GameplayPathActor를 찾아 넣어주기)
+
+public:
+    UFUNCTION(BlueprintCallable)
+    void Spawn();
+    
+    UPROPERTY(EditAnywhere)
+    TArray<TSubclassOf<AUnitCharacter>> SpawnUnits;
+
+    UFUNCTION(BlueprintCallable)
+    int32 GetCurrentWave();
+
+protected:
+
+private:
+    int32 CurrentWave;
+
+    UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+    AGameplayPathActor* GameplayPathActor;
 };
